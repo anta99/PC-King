407f65e99824f0adcc1efb635bbe7d3af82e853c
anta99 <48068151+anta99@users.noreply.github.com>
2019-11-16 16:39:59 +0100

    Filter products,contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,13 @@
+window.onload=function(){
+    
+    sign.addEventListener("click",newsLetter);
+  }
 $(document).ready(function(){
     navWrite(menu);
     slider(1);
     aboutWrite(about);
     bestSellersWrite("#wrapper",bestSellers,"bestSeler");
     bestSellersWrite("#wrapperProduct",bestSellers2,"product");
-    
    $("#menu li:nth-child(3)").on("click",function(){
        $("#podmeni").slideToggle();
    });
@@ -16,10 +19,11 @@ $(".textAbout").on("click",function(e){
         //$(e.currentTarget).find("img").slideToggle(); 
         $(e.currentTarget).find(".plus").toggleClass("plusActive");   
 });
+    
   });
 const menu=[{title:"Home",link:"#home"},{title:"About",link:"#about"},{title:"Products",link:"#products"},{title:"Contact",link:"#contact"}];
 const bestSellers=[{name:"PC King Energy MAX",price:"700$",img:"assets/images/bestseller1.png"},{name:"ASUS ROG Strix SCAR II",price:"1300$",img:"assets/images/bestseller2.jpg"},{name:"HP Gaming Pavilion",price:"900$",img:"assets/images/bestseller3.png"},{name:"PC King Prime Pro",price:"950$",img:"assets/images/bestseller4.png"}];
-const bestSellers2=[{name:"0ASUS ROG Strix SCAR II",price:"1300$",img:"assets/images/bestseller2.jpg"},{name:"1HP Gaming Pavilion",price:"900$",img:"assets/images/bestseller3.png"},{name:"2PC King Prime Pro",price:"950$",img:"assets/images/bestseller4.png"},{name:"3PC King Energy MAX",price:"700$",img:"assets/images/bestseller1.png"},{name:"4ASUS ROG Strix SCAR II",price:"1300$",img:"assets/images/bestseller2.jpg"},{name:"5HP Gaming Pavilion",price:"900$",img:"assets/images/bestseller3.png"},{name:"6PC King Prime Pro",price:"950$",img:"assets/images/bestseller4.png"}]
+const bestSellers2=[{name:"0ASUS ROG Strix SCAR II",price:"1300$",img:"assets/images/bestseller2.jpg",type:"laptop"},{name:"1HP Gaming Pavilion",price:"900$",img:"assets/images/bestseller3.png",type:"laptop"},{name:"2PC King Prime Pro",price:"950$",img:"assets/images/bestseller4.png",type:"pc"},{name:"3PC King Energy MAX",price:"700$",img:"assets/images/bestseller1.png",type:"pc"},{name:"4ASUS ROG Strix SCAR II",price:"1300$",img:"assets/images/bestseller2.jpg",type:"laptop"},{name:"5HP Gaming Pavilion",price:"900$",img:"assets/images/bestseller3.png",type:"laptop"},{name:"6PC King Prime Pro",price:"950$",img:"assets/images/bestseller4.png",type:"pc"}]
 const submenu=[{title:"Best sellers",link:"#bestSellers"},{title:"All Products",link:"#products"}];
 const sliderImages=["assets/images/slide1.jpg","assets/images/slide2.jpg","assets/images/slide3.jpg","assets/images/slide4.jpg","assets/images/slide5.jpg"];
 const messages=["Ovde ide neki naslov","Ovde ide neki naslov1","Ovde ide neki naslov2","Ovde ide neki naslov3","Ovde ide neki naslov4"];
@@ -108,25 +112,26 @@ function bestSellersWrite(target,array,identificator){
     for(let i in array){
         const article=document.createElement("article");
         article.setAttribute("class",identificator);
+        article.setAttribute("data-type",array[i].type);
         article.innerHTML+=`<img src="${array[i].img}" alt="bestSeller" />
         <h3>${array[i].name}</h3>
         <span>${array[i].price}</span>`
         const div=document.createElement("div");
         div.setAttribute("class","options")
-        div.innerHTML+=`<button class="buynow">Buy Now</button>
+        div.innerHTML+=`<button class="buynow" onclick="window.location.assign('contact.html')">Buy Now</button>
         <button class="details">Details</button>`
         article.appendChild(div);
         targetDiv.appendChild(article);
     }
-    const elements=document.querySelectorAll(`.${identificator}`);
-    console.log(elements);   
+    const elements=document.getElementsByClassName(identificator);
+    //console.log(elements);   
     for(let i=0;i<elements.length;i++){
         elements[i].addEventListener("click",function(){
             bodyOpacity().appendChild(detailsDiv(array,i));
             //document.querySelector("body").appendChild(detailsDiv(array,i));
+            close();
         })
     }
-    const closeBtns=document.getElementsByClassName("close");
 }
 function bodyOpacity(){
     const coverDiv=document.createElement("div");
@@ -135,16 +140,12 @@ function bodyOpacity(){
     document.querySelector("body").appendChild(coverDiv);
     return coverDiv;
 }
-function bodyOpacityRemove(){
-    document.querySelector("body").classList.remove("bodyAcitve");
-}
 function detailsDiv(array,i){
     var fixedDiv=document.createElement("div");
     fixedDiv.setAttribute("class","fixedDiv");
     fixedDiv.innerHTML+=`<div class="detaliName"><h2>${array[i].name}</h2></div>
     <div class="detailImg"><img src="${array[i].img}" alt="Product image" /></div>
-    <span class="close">Close</span>`;
-    console.log(`Clicked on element with index:${i}`);
+    <span class="close"><i class="fa fa-close"></i></span>`;
     return fixedDiv;
 }
 const filterButtons=document.querySelectorAll(".filterbtn");
@@ -157,18 +158,45 @@ for(let i=0; i< filterButtons.length;i++){
         
     })
 }
-function filter(targetDiv,con){
-    const target=document.querySelectorAll(targetDiv);
-    if(con==''){
-        console.log("Prikazi sve");
+function filter(con){
+    const target=document.querySelectorAll(".product");
+    for(let i=0;i<target.length;i++){
+        if(con==""){
+            target[i].style.display="block";
+            continue;
+        }
+        if(target[i].getAttribute("data-type")!=con){
+            target[i].style.display="none";  
+        }
+        else{
+            target[i].style.display="block";
+        }
     }
-    else if(con=='laptops'){
-        console.log("prikazi samo lapotopove");
+}
+function close(){
+    const closeBtns=document.querySelector(".close");
+    const fixedDiv=document.querySelector(".fixedDiv");
+    const coverDiv=document.querySelector("#coverDiv");
+        closeBtns.addEventListener("click",function(){
+            fixedDiv.remove();
+            coverDiv.remove();
+            document.querySelector("body").classList.remove("bodyAcitve");
+        });
+}
+function newsLetter(e){
+    e.preventDefault();
+    const input=document.querySelector("#mail");
+    const message=document.querySelector("#newsMessage");
+    const mailregex=/^[A-z][A-z\d]{4,}@(gmail|yahoo).com$/;
+    console.log(input.value);
+    if(mailregex.test(input.value)){
+        message.innerHTML="Thanks for subscribe on our newsleter";
+        console.log("mail je dobar");
     }
     else{
-        console.log("Prikazi Pc-jeve");
+        message.innerHTML="Enter mail in valid format";
+        console.log("Mail nije dobar");
     }
-    //console.log(target);
 }
 
 
